411504e46ef4a978a56f8d28f49811f793131818
alejandroal78 <alvarea12@outlook.com>
2021-09-02 15:29:44 +0200

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB5ycR0iJx_PohhTGRkR3KkuWA12L87kBU",
+    authDomain: "airbb-669c3.firebaseapp.com",
+    projectId: "airbb-669c3",
+    storageBucket: "airbb-669c3.appspot.com",
+    messagingSenderId: "923095593643",
+    appId: "1:923095593643:web:de6864cfa2b142bd167dee"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5ycR0iJx_PohhTGRkR3KkuWA12L87kBU",
+    authDomain: "airbb-669c3.firebaseapp.com",
+    projectId: "airbb-669c3",
+    storageBucket: "airbb-669c3.appspot.com",
+    messagingSenderId: "923095593643",
+    appId: "1:923095593643:web:de6864cfa2b142bd167dee"
+  }
 };
 
 /*
